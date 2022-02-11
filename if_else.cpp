5ec8b99bb65bb46b6a7cee251b5ed51e38473a3e
vinayak-bansode143 <93610471+vinayak-bansode143@users.noreply.github.com>
2022-02-11 23:08:37 +0530

    Add files via upload

---
+++ if_else.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if (n>0){
+        cout<<"number is positive"<<endl;
+    }else{
+        cout<<"Number is negative";
+    }
+    return 0;
+}
